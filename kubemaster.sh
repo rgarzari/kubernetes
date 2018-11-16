9ff564d9f9ee18d9cefc3da6ba96540fc953e51d
rgarzari <ramiro.gza@gmail.com>
2018-11-16 09:27:55 -0600

    Update kubemaster.sh

--- kubemaster.sh
+++ kubemaster.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# run this script as sudo, for example:
+# sudo ./kubemaster.sh
 
 # Install Docker from Ubuntu's repositories:
 #apt-get update
@@ -54,8 +56,20 @@ apt-get update
 # Used for flannel to pass bridged IPv4 traffic to iptables’ chains
 sysctl net.bridge.bridge-nf-call-iptables=1
 
-#!/bin/sh
-
 # Verify connectivity to gcr.io registries
 kubeadm config images pull
 
+# Initialize the master
+kubeadm init --apiserver-advertise-address=10.0.0.2 --pod-network-cidr=10.244.0.0/16
+
+# Add env variable
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
+# Copy the admin.conf file into $HOME/.kube/config
+  mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Install flannel - Might comment this one out for the lab so that the students
+# know how flannel is installed
+sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
