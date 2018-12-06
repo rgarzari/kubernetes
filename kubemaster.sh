604a3fc2ae07c2128ddf981875b9fab3c50e5211
rgarzari <ramiro.gza@gmail.com>
2018-12-06 12:00:00 -0600

    Update kubemaster.sh

--- kubemaster.sh
+++ kubemaster.sh
@@ -94,6 +94,7 @@ alias egrep='egrep --color=auto'
 if [ $commands[kubectl] ]; then
   source <(kubectl completion zsh)
 fi
+PROMPT='%F{cyan}%n%f@%F{cyan}%m%f %F{red}%1~%f %# '
 # The following two lines should be the last one in the .zshrc file
 # zsh-syntax-highlighting should be at the end of this file
 source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
