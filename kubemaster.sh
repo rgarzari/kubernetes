41b6c95e5f30b0db23701e16dedbcc116df37726
rgarzari <ramiro.gza@gmail.com>
2018-12-06 10:33:20 -0600

    Update kubemaster.sh

--- kubemaster.sh
+++ kubemaster.sh
@@ -86,15 +86,6 @@ cp /etc/skel/.bashrc ~/
 #export TERM=xterm-256color
 
 # Create .zshrc file for zsh
-#touch ~/.zshrc
-#echo "export TERM=linux" >> ~/.zshrc
-#echo "alias ls='ls --color=auto'" >> ~/.zshrc
-#echo "alias grep='grep --color=auto'" >> ~/.zshrc
-#echo "alias egrep='egrep --color=auto'" >> ~/.zshrc
-# The following two lines should be the last one in the .zshrc file
-#echo "# zsh-syntax-highlighting should be at the end of this file" >> ~/.zshrc
-#echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-
 cat <<EOF >~/.zshrc
 export TERM=linux
 alias ls='ls --color=auto'
@@ -107,7 +98,6 @@ fi
 # zsh-syntax-highlighting should be at the end of this file
 source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 EOF
-
 
 sudo chown $(id -u cisco):$(id -g cisco) $HOME/.zshrc
 # Make zsh the default shell for cisco user
