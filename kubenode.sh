7137d0dd50d24f18c855e9f67fbe3a83619f48d4
rgarzari <ramiro.gza@gmail.com>
2018-12-16 09:44:28 -0600

    Update kubenode.sh

--- kubenode.sh
+++ kubenode.sh
@@ -7,7 +7,7 @@
 # or install Docker CE 18.06 from Docker's repositories for Ubuntu or Debian:
 
 ## Install prerequisites.
-apt-get update && apt-get install -y apt-transport-https ca-certificates curl software-properties-common jq conntrack zsh zsh-syntax-highlighting bridge-utils highlight
+apt-get update && apt-get install -y apt-transport-https ca-certificates curl software-properties-common jq conntrack zsh zsh-syntax-highlighting bridge-utils
 
 ## Download GPG key.
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
@@ -76,7 +76,6 @@ setopt share_history
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
-alias cat='highlight -O ansi --force'
 # kubectl not needed for workers/minions
 #if [ /usr/bin/kubectl ]; then
 #  source <(kubectl completion zsh)
