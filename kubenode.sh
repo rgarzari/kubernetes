8001a80dfe23edaf7881a7852e84759114bb1067
rgarzari <ramiro.gza@gmail.com>
2019-05-07 14:14:02 -0500

    Update kubenode.sh

--- kubenode.sh
+++ kubenode.sh
@@ -46,7 +46,7 @@ cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt-get update
-apt-get install -y kubelet=1.12.2-00 kubeadm=1.12.2-00 kubectl=1.12.2-00
+apt-get install -y kubelet=1.12.2-00 kubeadm=1.12.2-00 kubectl=1.12.2-00 kubernetes-cni=0.6.0-00
 apt-mark hold kubelet kubeadm kubectl
 
 apt-get update
